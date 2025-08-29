d5f26cc86d113bc9f66991ee79253bd570fb3e9d
Son Tran <sonthanhtran2004@gmail.com>
2025-08-29 19:29:20 +0700

    Separate new models into packages

---
+++ backend/app/models/user.py
@@ -0,0 +1,70 @@
+import uuid
+from typing import TYPE_CHECKING
+
+from pydantic import EmailStr
+from sqlmodel import Field, Relationship, SQLModel
+
+
+if TYPE_CHECKING:
+    from .item import Item
+    from .request import Request, RequestUser
+    from .chat import ChatMessage, ChatUser
+
+# Shared properties
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    is_active: bool = True
+    is_superuser: bool = False
+    username: str | None = Field(default=None, max_length=255)
+    
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=40)
+
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+    full_name: str | None = Field(default=None, max_length=255)
+
+
+# Properties to receive via API on update, all are optional
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    password: str | None = Field(default=None, min_length=8, max_length=40)
+
+
+class UserUpdateMe(SQLModel):
+    username: str | None = Field(default=None, max_length=255)
+    email: EmailStr | None = Field(default=None, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+
+
+class UpdatePassword(SQLModel):
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    __tablename__ = "users"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hashed_password: str
+    chat_memberships: list["ChatUser"] = Relationship(back_populates="user", cascade_delete=True) 
+    request_memberships: list["RequestUser"] = Relationship(back_populates="user") # Join table for requests and users
+    owned_requests: list["Request"] = Relationship(back_populates="owner") 
+    sent_messages: list["ChatMessage"] = Relationship(back_populates="sender") 
+    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+
+
+# Properties to return via API, id is always required
+class UserPublic(UserBase):
+    id: uuid.UUID
+
+
+class UsersPublic(SQLModel):
+    data: list[UserPublic]
+    count: int
