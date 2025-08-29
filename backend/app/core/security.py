4c83d6a3823456a1ebb51f420b590ca80b15b377
chezzijr <nhuthuan0501@gmail.com>
2025-08-29 18:33:00 +0700

    fix: use utf-8 instead of latin-1

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -18,11 +18,11 @@ def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
-        plain_password.encode("utf-8"), hashed_password.encode("latin-1")
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
 
 
 def get_password_hash(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
-    return hashed.decode("latin-1")
+    return hashed.decode("utf-8")
