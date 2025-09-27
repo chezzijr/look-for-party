ab66cc2bde11e635c9ef2beba962b0e864ae3499
chezzijr <nhuthuan0501@gmail.com>
2025-09-28 01:32:40 +0700

    fix: redirect to login page when token expired

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -24,6 +24,7 @@ const useAuth = () => {
     queryKey: ["currentUser"],
     queryFn: UsersService.readUserMe,
     enabled: isLoggedIn(),
+    retry: false,
   })
 
   const signUpMutation = useMutation({
